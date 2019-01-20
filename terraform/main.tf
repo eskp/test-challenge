d01193149a8a6f0b77139d4153a00bb8cba9b0e5
Simon KP <simon.kp@amazee.com>
2019-01-20 23:16:11 +1100

    Rename credentials profile to default

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,5 +2,5 @@ provider "aws" {
   version                 = "~> 1.15"
   region                  = "${var.region}"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "test"
+  profile                 = "default"
 }
